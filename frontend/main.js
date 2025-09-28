6b795923374dbfbda0513f9c5b2df4be879b53de
kundanr2 <s223924482@deakin.edu.au>
2025-09-28 14:52:52 +1000

    t1h

--- frontend/main.js
+++ frontend/main.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // API endpoints for the Product, Order, and Customer services.
     // These ports are mapped from the Docker containers to the host machine in docker-compose.yml.
-    const PRODUCT_API_BASE_URL  = "http://<product-service-external-ip>:8000";
+    const PRODUCT_API_BASE_URL  = "http://20.167.26.114:8000";
     const ORDER_API_BASE_URL    = 'http://order-service:8001';
     const CUSTOMER_API_BASE_URL = 'http://customer-service:8002'; 
 
