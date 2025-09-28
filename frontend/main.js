815da1de03fc9a51de382cd10577895ec679669d
kundanr2 <s223924482@deakin.edu.au>
2025-09-28 12:29:37 +1000

    Updated main.js to ClusterIP, switched to sit72210dacrstu ACR, updated K8s manifests

--- frontend/main.js
+++ frontend/main.js
@@ -3,9 +3,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     // API endpoints for the Product, Order, and Customer services.
     // These ports are mapped from the Docker containers to the host machine in docker-compose.yml.
-    const PRODUCT_API_BASE_URL = 'http://<product-external-ip>:8000';
-    const ORDER_API_BASE_URL   = 'http://<order-external-ip>:8001';
-    const CUSTOMER_API_BASE_URL = 'http://<customer-external-ip>:8002';  
+    const PRODUCT_API_BASE_URL  = 'http://product-service:8000';
+    const ORDER_API_BASE_URL    = 'http://order-service:8001';
+    const CUSTOMER_API_BASE_URL = 'http://customer-service:8002'; 
 
     // DOM Elements
     const messageBox = document.getElementById('message-box');
